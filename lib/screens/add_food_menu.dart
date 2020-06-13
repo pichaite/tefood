03bc97d3cd19c99d3098afd9a109b16024a2f7f1
Pichai Yodcahd <pichaiaot@gmail.com>
2020-06-13 20:54:02 +0700

    Master

--- lib/screens/add_food_menu.dart
+++ lib/screens/add_food_menu.dart
@@ -1,5 +1,13 @@
+import 'dart:io';
+import 'dart:math';
+
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:tefood/utility/my_constant.dart';
 import 'package:tefood/utility/my_style.dart';
+import 'package:tefood/utility/normal_dialog.dart';
 
 class AddFoodMenu extends StatefulWidget {
   @override
@@ -7,6 +15,9 @@ class AddFoodMenu extends StatefulWidget {
 }
 
 class _AddFoodMenuState extends State<AddFoodMenu> {
+  File file;
+  String nameFood, price, detail;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -39,7 +50,23 @@ class _AddFoodMenuState extends State<AddFoodMenu> {
       width: MediaQuery.of(context).size.width,
       child: RaisedButton.icon(
         color: MyStyle().primaryColor,
-        onPressed: () {},
+        onPressed: () {
+          if (file == null) {
+            narmalDialog(context,
+                'กรุณาเลือกรูปภาพ อาหาร โดยการ Tap Camera หรือ Gallery');
+          } else if (nameFood == null ||
+              nameFood.isEmpty ||
+              price == null ||
+              price.isEmpty ||
+              detail == null ||
+              detail.isEmpty) {
+            narmalDialog(context, 'กรุณากรอก ทุกช่อง ค่ะ');
+          } else {
+            print(
+                'nameFood = $nameFood, price = $price, detail = $detail Image = ${file.path}');
+            uploadFoodAndInsertData();
+          }
+        },
         icon: Icon(
           Icons.save,
           color: Colors.white,
@@ -52,9 +79,31 @@ class _AddFoodMenuState extends State<AddFoodMenu> {
     );
   }
 
+  Future<Null> uploadFoodAndInsertData() async {
+    String urlUpload = '${MyConstant().domain}/tefood/saveFood.php';
+    Random random = Random();
+    int i = random.nextInt(1000000);
+    String nameFile = 'food$i.jpg';
+    try {
+      Map<String, dynamic> map = Map();
+      map['file'] = await MultipartFile.fromFile(file.path, filename: nameFile);
+      FormData formData = FormData.fromMap(map);
+      await Dio().post(urlUpload, data: formData).then((value) async {
+        String urlPathImage = '/tefood/Food/$nameFile';
+        print('urlPathImage = ${MyConstant().domain}$urlPathImage');
+        SharedPreferences preferences = await SharedPreferences.getInstance();
+        String idShop = preferences.getString('id');
+        String urlInsertData =
+            '${MyConstant().domain}/tefood/addFood.php?isAdd=true&idShop=$idShop&NameFood=$nameFood&PathImage=$urlPathImage&Price=$price&Detail=$detail';
+        await Dio().get(urlInsertData).then((value) => Navigator.pop(context));
+      });
+    } catch (e) {}
+  }
+
   Widget nameForm() => Container(
         width: 250.0,
         child: TextField(
+          onChanged: (value) => nameFood = value.trim(),
           decoration: InputDecoration(
             prefixIcon: Icon(Icons.fastfood),
             labelText: 'ชื่ออาหาร',
@@ -66,6 +115,7 @@ class _AddFoodMenuState extends State<AddFoodMenu> {
   Widget priceForm() => Container(
         width: 250.0,
         child: TextField(
+          onChanged: (value) => price = value.trim(),
           keyboardType: TextInputType.number,
           decoration: InputDecoration(
             prefixIcon: Icon(Icons.attach_money),
@@ -78,6 +128,7 @@ class _AddFoodMenuState extends State<AddFoodMenu> {
   Widget detailForm() => Container(
         width: 250.0,
         child: TextField(
+          onChanged: (value) => detail = value.trim(),
           keyboardType: TextInputType.multiline,
           maxLines: 3,
           decoration: InputDecoration(
@@ -92,15 +143,38 @@ class _AddFoodMenuState extends State<AddFoodMenu> {
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceAround,
       children: [
-        IconButton(icon: Icon(Icons.add_a_photo), onPressed: null),
+        IconButton(
+          icon: Icon(Icons.add_a_photo),
+          onPressed: () => chooseImage(ImageSource.camera),
+        ),
         Container(
           width: 250.0,
           height: 250.0,
-          child: Image.asset('assets/images/picfood.png'),
+          child: file == null
+              ? Image.asset('assets/images/picfood.png')
+              : Image.file(file),
         ),
-        IconButton(icon: Icon(Icons.add_photo_alternate), onPressed: null),
+        IconButton(
+          icon: Icon(Icons.add_photo_alternate),
+          onPressed: () => chooseImage(ImageSource.gallery),
+        ),
       ],
     );
+  }
+
+  Future<Null> chooseImage(ImageSource source) async {
+    try {
+      var object = await ImagePicker().getImage(
+        source: source,
+        maxWidth: 800.0,
+        maxHeight: 800.0,
+      );
+      setState(() {
+        file = File(object.path);
+      });
+    } catch (e) {
+      print(e);
+    }
   }
 
   Widget showTitleTopFood(String string) {
--- lib/widget/list_food_menu_shop.dart
+++ lib/widget/list_food_menu_shop.dart
@@ -1,5 +1,12 @@
+import 'dart:convert';
+
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:tefood/model/food_model.dart';
 import 'package:tefood/screens/add_food_menu.dart';
+import 'package:tefood/utility/my_constant.dart';
+import 'package:tefood/utility/my_style.dart';
 
 class ListFoodMenuShop extends StatefulWidget {
   @override
@@ -7,12 +14,106 @@ class ListFoodMenuShop extends StatefulWidget {
 }
 
 class _ListFoodMenuShopState extends State<ListFoodMenuShop> {
+  bool status = true;
+  bool loadStatus = true;
+  List<FoodModel> foodmodels = List();
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    readFoodMenu();
+  }
+
+  Future<Null> readFoodMenu() async {
+
+    if (foodmodels.length != 0) {
+      foodmodels.clear();
+    }
+
+    SharedPreferences preferences = await SharedPreferences.getInstance();
+    String idShop = preferences.getString('id');
+    String url =
+        '${MyConstant().domain}/tefood/getFoodWhereIdShop.php?isAdd=true&idShop=$idShop';
+    await Dio().get(url).then((value) {
+      setState(() {
+        loadStatus = false;
+      });
+
+      if (value.toString() != 'null') {
+        var result = json.decode(value.data);
+        // print('Result ===>>> $result');
+
+        for (var map in result) {
+          FoodModel foodModel = FoodModel.fromJson(map);
+          setState(() {
+            foodmodels.add(foodModel);
+          });
+        }
+      } else {
+        setState(() {
+          status = false;
+        });
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Stack(
-      children: [Text('รายการอาหารร้าน'), addMenuButton()],
+      children: [
+        loadStatus ? MyStyle().showProgress() : showContent(),
+        addMenuButton(),
+      ],
     );
   }
+
+  Widget showContent() {
+    return status
+        ? shoeListFood()
+        : Center(
+            child: Text('ยังไม่มีรายการอาหาร'),
+          );
+  }
+
+  Widget shoeListFood() => ListView.builder(
+        itemCount: foodmodels.length,
+        itemBuilder: (context, index) => Row(
+          children: [
+            Container(
+              padding: EdgeInsets.all(10.0),
+              width: MediaQuery.of(context).size.width * 0.5,
+              height: MediaQuery.of(context).size.width * 0.4,
+              child: Image.network(
+                '${MyConstant().domain}${foodmodels[index].pathImage}',
+                fit: BoxFit.cover,
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.all(10.0),
+              width: MediaQuery.of(context).size.width * 0.5,
+              height: MediaQuery.of(context).size.width * 0.4,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  MyStyle().showTitleH2(foodmodels[index].nameFood),
+                  Text(
+                    'ราคา ${foodmodels[index].price} บาท',
+                    style: TextStyle(
+                      color: Colors.green.shade800,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text(
+                    foodmodels[index].detail,
+                    style: TextStyle(),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      );
 
   Widget addMenuButton() => Column(
         mainAxisAlignment: MainAxisAlignment.end,
@@ -27,8 +128,10 @@ class _ListFoodMenuShopState extends State<ListFoodMenuShop> {
                 ),
                 child: FloatingActionButton(
                   onPressed: () {
-                    MaterialPageRoute materialPageRoute = MaterialPageRoute(builder: (context) => AddFoodMenu());
-                    Navigator.push(context, materialPageRoute);
+                    MaterialPageRoute materialPageRoute =
+                        MaterialPageRoute(builder: (context) => AddFoodMenu());
+                    Navigator.push(context, materialPageRoute)
+                        .then((value) => readFoodMenu());
                   },
                   child: Icon(Icons.add),
                 ),
